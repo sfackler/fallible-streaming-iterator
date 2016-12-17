fcd2b0332641a53fccfb01e2c5717ddea51e2579
Steven Fackler <sfackler@palantir.com>
2016-12-16 17:08:45 -0800

    Some methods

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,50 @@ pub trait FallibleStreamingIterator {
         self.advance()?;
         Ok(self.get())
     }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, None)
+    }
+
+    #[inline]
+    fn all<F>(&mut self, mut f: F) -> Result<bool, Self::Error>
+        where Self: Sized,
+              F: FnMut(&Self::Item) -> bool
+    {
+        while let Some(e) = self.next()? {
+            if !f(e) {
+                return Ok(false);
+            }
+        }
+        Ok(true)
+    }
+
+    #[inline]
+    fn any<F>(&mut self, mut f: F) -> Result<bool, Self::Error>
+        where Self: Sized,
+              F: FnMut(&Self::Item) -> bool
+    {
+        self.all(|e| !f(e)).map(|r| !r)
+    }
+
+    #[inline]
+    fn by_ref(&mut self) -> &mut Self
+        where Self: Sized
+    {
+        self
+    }
+
+    #[inline]
+    fn count(mut self) -> Result<usize, Self::Error>
+        where Self: Sized
+    {
+        let mut count = 0;
+        while let Some(_) = self.next()? {
+            count += 1;
+        }
+        Ok(count)
+    }
 }
 
 #[cfg(test)]
