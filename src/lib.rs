dbdfb8eb196e0af543245921116a9f7da9543a9a
Steven Fackler <sfackler@palantir.com>
2016-12-16 16:33:01 -0800

    Trait basics

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+#![no_std]
+
+pub trait FallibleStreamingIterator {
+    type Item: ?Sized;
+    type Error;
+
+    fn advance(&mut self) -> Result<(), Self::Error>;
+
+    fn get(&self) -> Option<&Self::Item>;
+
+    #[inline]
+    fn next(&mut self) -> Result<Option<&Self::Item>, Self::Error> {
+        self.advance()?;
+        Ok(self.get())
     }
 }
